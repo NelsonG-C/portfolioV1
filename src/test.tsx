e951fa75476cbd77da4eddcfff77ed8d1a2d297e
Nelson Gardner-Challis <nelsongc59@gmail.com>
2020-12-30 12:43:39 +1100

    svg trial added

---
+++ src/test.tsx
@@ -0,0 +1,28 @@
+import React, { CSSProperties } from "react"
+import "./styles/style.css"
+
+export const SVGTest: any = () => {
+  const styleSVG: CSSProperties = {
+    fill: "#000000",
+    strokeWidth: "0.264583",
+  }
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      id="svg8"
+      version="1.1"
+      viewBox="0 0 210 297"
+      height="297mm"
+      width="210mm"
+    >
+      <defs id="defs2" />
+      <g id="layer1">
+        <path
+          d="M 105.07738,106.58928 83.358063,95.475813 61.89004,107.06727 65.747939,82.976722 48.089822,66.141377 72.193453,62.366067 82.74816,40.369792 l 11.038964,21.757281 24.181286,3.2409 -17.28118,17.222049 z"
+          id="path10"
+          style={styleSVG}
+        />
+      </g>
+    </svg>
+  )
+}
