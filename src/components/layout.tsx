01ab0ce7ca83232a5ccfb2ffa9330d844095cd25
Nelson Gardner-Challis <nelsongc59@gmail.com>
2021-01-12 20:31:51 +1100

    removed clashing css

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -10,9 +10,11 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
+import ThemeToggle from "./themeToggle"
+
 import "./layout.css"
 
-const Layout = ({ children }) => {
+const Layout = ({ children }: any) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -33,10 +35,15 @@ const Layout = ({ children }) => {
           padding: `0 1.0875rem 1.45rem`,
         }}
       >
-        <main>{children}</main>
-        <footer style={{
-          marginTop: `2rem`
-        }}>
+        <main>
+          <ThemeToggle />
+          <div>{children}</div>
+        </main>
+        <footer
+          style={{
+            marginTop: `2rem`,
+          }}
+        >
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.com">Gatsby</a>
