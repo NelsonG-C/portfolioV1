268bacd767023a96b73568ec3b96b839dba75fd6
Nelson Gardner-Challis <nelsongc59@gmail.com>
2021-01-12 20:31:29 +1100

    theme toggle component complete

---
+++ src/components/themeToggle.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import "../styles/style.css";
+
+const ThemeToggle = () => {
+  const [checked, setChecked] = useState("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { checked } = e.target;
+    const body = document.body.classList;
+    if (checked) {
+      body.add("lighttheme");
+      setChecked("move");
+    } else {
+      body.remove("lighttheme");
+      setChecked("");
+    }
+  };
+
+  return (
+    <div className={`switch ${checked !== "" ? "move" : ""}`}>
+      <input type="checkbox" onChange={e => handleChange(e)} />
+      <span className="circle large"></span>
+      <span className="circle small"></span>
+    </div>
+  );
+};
+
+export default ThemeToggle;
