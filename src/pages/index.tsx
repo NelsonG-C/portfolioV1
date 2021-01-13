6e2d420d08519055c4742413acfb0f1f03b2149d
Nelson Gardner-Challis <nelsongc59@gmail.com>
2021-01-13 19:42:54 +1100

    implemented mouseover event

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,38 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { Link } from "gatsby";
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import Image from "../components/image";
+import SEO from "../components/seo";
 
-import { SVGTest } from "../test"
+import { SVGTest } from "../test";
+import "../styles/style.css";
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <SVGTest />
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  const handleEnter = () => {
+    document.body.classList.add("lighttheme");
+  };
 
-export default IndexPage
+  const handleExit = () => {
+    document.body.classList.remove("lighttheme");
+  };
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1 onMouseEnter={handleEnter} onMouseLeave={handleExit}>
+        Hi people
+      </h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <SVGTest />
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link> <br />
+      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    </Layout>
+  );
+};
+
+export default IndexPage;
