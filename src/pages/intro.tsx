99c3293fed4bde065511f051b403bb168fef3476
Nelson Gardner-Challis <nelsongc59@gmail.com>
2021-01-14 20:07:25 +1100

    basic intro page component

---
+++ src/pages/intro.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+// Images
+
+const Intro = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <img src="" alt="Animated profile image" />
+        </div>
+        <div className="col">
+          <h1>Hi I'm Nelson</h1>
+          <h2>Full Stack Developer, based in Australia</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
