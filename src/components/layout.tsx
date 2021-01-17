dcbe3c5fd8f681ae7b2a00ce94930995c296986e
Nelson Gardner-Challis <nelsongc59@gmail.com>
2021-01-17 13:26:48 +1100

    added new font

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,14 +5,14 @@
  * See: https://www.gatsbyjs.com/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import React from "react";
+import PropTypes from "prop-types";
+import { useStaticQuery, graphql } from "gatsby";
 
-import Header from "./header"
-import ThemeToggle from "./themeToggle"
+import Header from "./header";
+import ThemeToggle from "./themeToggle";
 
-import "./layout.css"
+import "./layout.css";
 
 const Layout = ({ children }: any) => {
   const data = useStaticQuery(graphql`
@@ -23,18 +23,12 @@ const Layout = ({ children }: any) => {
         }
       }
     }
-  `)
+  `);
 
   return (
     <>
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+      <div>
         <main>
           <ThemeToggle />
           <div>{children}</div>
@@ -50,11 +44,11 @@ const Layout = ({ children }: any) => {
         </footer>
       </div>
     </>
-  )
-}
+  );
+};
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
-export default Layout
+export default Layout;
