f0e3877ee5d3e30ada2049f8a40533928bda69c5
Nelson Gardner-Challis <nelsongc59@gmail.com>
2021-01-17 12:11:29 +1100

    basic contact page added

---
+++ src/pages/contact.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="container">
+      <h2 className="contact-heading">Get In Touch</h2>
+      <p className="contact-para">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus
+        accusantium nihil nesciunt facilis, quas delectus nulla aperiam unde?
+        Nesciunt, temporibus?
+      </p>
+    </div>
+  );
+};
+
+export default Contact;
