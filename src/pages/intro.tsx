d520883bb1c6b38c0038fddd022ec7b515739d88
Nelson Gardner-Challis <nelsongc59@gmail.com>
2021-01-18 19:59:10 +1100

    styling layout update

--- src/pages/intro.tsx
+++ src/pages/intro.tsx
@@ -8,16 +8,18 @@ import "../styles/style.css";
 
 const Intro = () => {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col">
-          <div className="headshot">
-            <Image name="headshot" />
+    <div className="intro-block">
+      <div className="container">
+        <div className="row">
+          <div className="headshot-col col">
+            <div className="headshot">
+              <Image name="headshot" />
+            </div>
           </div>
-        </div>
-        <div className="col">
-          <h1>Hi I'm Nelson</h1>
-          <h2>Full Stack Developer, based in Australia</h2>
+          <div className="intro-desc col">
+            <h1>Hi I'm Nelson</h1>
+            <h2>Full Stack Developer, based in Australia</h2>
+          </div>
         </div>
       </div>
     </div>
