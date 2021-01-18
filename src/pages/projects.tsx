06b1ecf7ae38d25352783330b55255d0fe995b51
Nelson Gardner-Challis <nelsongc59@gmail.com>
2021-01-18 16:34:08 +1100

    graphql query and mapping data

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,35 +1,86 @@
 import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
 
 import Image from "../components/image";
+import Img from "gatsby-image";
 import "../styles/style.css";
 // Project Data
 import data from "../data/projects";
 
 const Projects = () => {
+  const data = useStaticQuery(graphql`
+    {
+      projects: allMarkdownRemark(
+        filter: { fileAbsolutePath: { regex: "/projects/" } }
+      ) {
+        edges {
+          node {
+            frontmatter {
+              title
+              photo {
+                childImageSharp {
+                  fluid(maxWidth: 600) {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
+              }
+              date
+              github
+              stack
+              site
+            }
+            html
+          }
+        }
+      }
+    }
+  `);
+  const projects = data.projects.edges.filter(({ node }: any) => node);
+  console.log(projects);
+
+  fetch("https://api.github.com/users/NelsonG-C")
+    .then(response => {
+      const result = response.json();
+      return result;
+    })
+    .then(data => {
+      console.log(data);
+    })
+    .catch(err => console.error(err));
   return (
     <div className="project-container">
-      <div className="row">
-        <div className="col">
-          <div className="project-img">
-            <Image name="testImage"></Image>
-          </div>
-        </div>
-        <div className="col">
-          <div className="project-card">
-            <h3 className="project-title">Test Title</h3>
-            <h4 className="project-info">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil sed
-              facilis quos harum beatae natus illo quod delectus perspiciatis
-              eaque.
-            </h4>
-            <h4 className="project-tags">Test Tag</h4>
-            <div className="project-btns">
-              <button className="btn-project">Github</button>
-              <button className="btn-project">Website</button>
+      {projects.map((project: any, i: number) => {
+        const { frontmatter, html } = project.node;
+        console.log(project);
+        const { title, photo, date, github, stack, site } = frontmatter;
+        return (
+          <div key={i} className="row">
+            <div className="col">
+              <div className="project-img">
+                <Img
+                  fluid={photo.childImageSharp.fluid}
+                  className="project-img"
+                />
+              </div>
+            </div>
+            <div className="col">
+              <div className="project-card">
+                <h3 className="project-title">{title}</h3>
+                <h4 className="project-info">{html}</h4>
+                <h4 className="project-tags">{stack}</h4>
+                <div className="project-btns">
+                  <button className="btn-project">
+                    <a href={github}>Github</a>
+                  </button>
+                  <button className="btn-project">
+                    <a href={site}>Website</a>
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
+        );
+      })}
     </div>
   );
 };
