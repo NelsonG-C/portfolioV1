169d8355ce5479f05e0fca6df35b56bdccfa5e3d
Nelson Gardner-Challis <nelsongc59@gmail.com>
2021-01-18 19:58:49 +1100

    background svg add

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,6 +26,17 @@ const IndexPage = () => {
   return (
     <Layout>
       <SEO title="Home" />
+      <svg
+        className="blob-svg"
+        viewBox="0 0 200 200"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fill="#FF0066"
+          d="M61.2,-15.9C70.3,8.1,62.4,41.5,42.3,55.6C22.1,69.6,-10.3,64.2,-34.2,46.8C-58.1,29.5,-73.6,0.3,-66.4,-20.9C-59.3,-42.2,-29.7,-55.5,-1.8,-54.9C26.1,-54.3,52.2,-39.9,61.2,-15.9Z"
+          transform="translate(100 100)"
+        />
+      </svg>
       <Intro />
       <Projects />
       <Contacts />
