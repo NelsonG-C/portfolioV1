1d62eee7b4b988ead811b9ad1066b9434a840a4c
Nelson Gardner-Challis <nelsongc59@gmail.com>
2021-01-19 15:48:53 +1100

    contact positioning class update

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -3,12 +3,16 @@ import React from "react";
 const Contact = () => {
   return (
     <div className="container">
-      <h2 className="contact-heading">Get In Touch</h2>
-      <p className="contact-para">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus
-        accusantium nihil nesciunt facilis, quas delectus nulla aperiam unde?
-        Nesciunt, temporibus?
-      </p>
+      <div className="row">
+        <div className="contact-block">
+          <h2 className="contact-heading">Get In Touch</h2>
+          <p className="contact-para">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus
+            accusantium nihil nesciunt facilis, quas delectus nulla aperiam
+            unde? Nesciunt, temporibus?
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
